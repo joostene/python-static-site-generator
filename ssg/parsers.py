1ff988a4affa7cce5aa71b2d7f632ecafae22c33
joostene <94166797+joostene@users.noreply.github.com>
2021-11-15 12:13:57 +0100

    2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,15 +1,16 @@
 import shutil
+
 from typing import List
 from pathlib import Path
 
+
 class Parser:
-      #initialize variables
-   extensions:List[str] = []
+   extensions: List[str] = []
 
    def valid_extension(self, extension):
       return extension in self.extensions
 
-   def parse (self, path:Path, source:Path, dest:Path):
+   def parse (self, path: Path, source: Path, dest: Path):
       raise NotImplementedError
 
    def read(self, path):
@@ -19,7 +20,7 @@ class Parser:
    def write(self, path, dest, content, ext = ".html"):
       full_path = dest / with_suffix(ext).name
       with open(full_path, "w") as file:
-         return file.write()
+         file.write(content)
 
    def copy(self, path, source, dest):
       shutil.copy2(path, dest / path.relative_to(source))
