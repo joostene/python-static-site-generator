3a67734e94ea919b420b322f6136988534e7dcd1
joostene <94166797+joostene@users.noreply.github.com>
2021-11-15 12:04:47 +0100

    last changes parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,41 +3,32 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-   def __init__(self, extensions):
       #initialize variables
-      extensions = []
-      extensions:List[str]
+      extensions:List[str] = []
 
    def valid_extension(self, extension):
-      if self.extension in self.extensions:
-         return self.extension
+      return self.extension in self.extensions
 
-   def parse (self, path, source, dest):
-      self.path:Path
-      self.source:Path
-      self.dest:Path
-      raise NotImplementedError("No Path")
+   def parse (self, path:Path, source:Path, dest:Path):
+      raise NotImplementedError
 
    def read(self, path):
-      with open(self.path, 'r') as file:
+      with open(path, "r" ) as file:
          return file.read()
 
-   def write(self, path, dest, content):
-      ext = ".html"
-      full_path = (self.dest / with_suffix(self.path).name / ext)
+   def write(self, path, dest, content, ext = ".html"):
+      full_path = dest / with_suffix(ext).name
+      with open(full_path, "w") as file:
+         return file.write()
 
    def copy(self, path, source, dest):
       shutil.copy2(path, dest / path.relative_to(source))
 
 
-   class ResourceParser:
+class ResourceParser(Parser):
 
-      extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+   extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-      def parse (self, path, source, dest):
-         self.path:Path
-         self.source:Path
-         self.dest:Path
-         raise NotImplementedError("No Path")
-         Parser.copy(self.path, self.source, self.dest)
+   def parse (self, path, source, dest):
+      self.copy(path, source, dest)
 
